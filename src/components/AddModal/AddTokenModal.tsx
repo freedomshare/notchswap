d96e0ecc6ac64e19398b5ab5062ed42dae868367
sphinx <corasphinxy@gmail.com>
2021-11-19 09:22:53 +0000

    complete AddTokenModal Design

--- src/components/AddModal/AddTokenModal.tsx
+++ src/components/AddModal/AddTokenModal.tsx
@@ -1,20 +1,23 @@
 import React, { useCallback, useState } from 'react'
 import { Currency, Token } from '@pancakeswap/sdk'
 import {
-  ModalContainer,
-  ModalHeader,
-  ModalTitle,
-  ModalBackButton,
-  ModalCloseButton,
-  ModalBody,
-  InjectedModalProps,
-  Heading,
-  Button,
+    ModalContainer,
+    ModalHeader,
+    ModalTitle,
+    ModalBackButton,
+    ModalCloseButton,
+    ModalBody,
+    InjectedModalProps,
+    Heading,
+    Button,
+    Input
 } from '@pancakeswap/uikit'
 import styled from 'styled-components'
+import { useTranslation } from 'contexts/Localization'
 import usePrevious from 'hooks/usePreviousValue'
 import { TokenList } from '@uniswap/token-lists'
-import { useTranslation } from 'contexts/Localization'
+import Column, { AutoColumn } from '../Layout/Column'
+import Row from '../Layout/Row'
 
 const Footer = styled.div`
   width: 100%;
@@ -32,44 +35,115 @@ const StyledModalBody = styled(ModalBody)`
 `
 
 interface AddTokenModalProps extends InjectedModalProps {
-  selectedCurrency?: Currency | null
-  onCurrencySelect: (currency: Currency) => void
-  otherSelectedCurrency?: Currency | null
-  showCommonBases?: boolean
+    selectedCurrency?: Currency | null
+    onCurrencySelect: (currency: Currency) => void
+    otherSelectedCurrency?: Currency | null
+    showCommonBases?: boolean
 }
 
 export default function AddTokenModal({
-  onDismiss = () => null,
-  onCurrencySelect,
-  selectedCurrency,
-  otherSelectedCurrency,
-  showCommonBases = false,
+    onDismiss = () => null,
+    onCurrencySelect,
+    selectedCurrency,
+    otherSelectedCurrency,
+    showCommonBases = false,
 }: AddTokenModalProps) {
 
-  const { t } = useTranslation()
+    const { t } = useTranslation()
 
-  
-  return (
-    <StyledModalContainer minWidth="320px">
-      <ModalHeader>
-        <ModalTitle>
-          <Heading>{t('Add Token')}</Heading>
-        </ModalTitle>
-        <ModalCloseButton onDismiss={onDismiss} />
-      </ModalHeader>
-      <StyledModalBody>
-        
-          <Footer>
-            <Button
-              scale="sm"
-              variant="text"
-            //   onClick={() => setModalView(CurrencyModalView.manage)}
-              className="list-token-manage-button"
-            >
-              {t('Add')}
-            </Button>
-          </Footer>
-      </StyledModalBody>
-    </StyledModalContainer>
-  )
+
+    return (
+        <StyledModalContainer minWidth="320px">
+            <ModalHeader>
+                <ModalTitle>
+                    <Heading>{t('Add Token')}</Heading>
+                </ModalTitle>
+                <ModalCloseButton onDismiss={onDismiss} />
+            </ModalHeader>
+            <StyledModalBody>
+                <AutoColumn gap="16px">
+                    <Row>
+                        <Input
+                            id="token-name"
+                            placeholder={t('Name')}
+                            scale="lg"
+                            autoComplete="off"
+                            // value={searchQuery}
+                            // ref={inputRef as RefObject<HTMLInputElement>}
+                            // onChange={handleInput}
+                            // onKeyDown={handleEnter}
+                        />
+                    </Row>
+                    <Row>
+                        <Input
+                            id="token-symbol"
+                            placeholder={t('Symbol')}
+                            scale="lg"
+                            autoComplete="off"
+                            // value={searchQuery}
+                            // ref={inputRef as RefObject<HTMLInputElement>}
+                            // onChange={handleInput}
+                            // onKeyDown={handleEnter}
+                        />
+                    </Row>
+                    <Row>
+                        <Input
+                            id="token-address"
+                            placeholder={t('Address')}
+                            scale="lg"
+                            autoComplete="off"
+                            // value={searchQuery}
+                            // ref={inputRef as RefObject<HTMLInputElement>}
+                            // onChange={handleInput}
+                            // onKeyDown={handleEnter}
+                        />
+                    </Row>
+                    <Row>
+                        <Input
+                            id="token-chainId"
+                            placeholder={t('ChainID')}
+                            scale="lg"
+                            autoComplete="off"
+                            // value={searchQuery}
+                            // ref={inputRef as RefObject<HTMLInputElement>}
+                            // onChange={handleInput}
+                            // onKeyDown={handleEnter}
+                        />
+                        <Input
+                            id="token-decimals"
+                            placeholder={t('Decimals')}
+                            scale="lg"
+                            autoComplete="off"
+                            // value={searchQuery}
+                            // ref={inputRef as RefObject<HTMLInputElement>}
+                            // onChange={handleInput}
+                            // onKeyDown={handleEnter}
+                        />
+                    </Row>
+                    <Row>
+                        <Input
+                            id="token-logouri"
+                            placeholder={t('LogoURI')}
+                            scale="lg"
+                            autoComplete="off"
+                            // value={searchQuery}
+                            // ref={inputRef as RefObject<HTMLInputElement>}
+                            // onChange={handleInput}
+                            // onKeyDown={handleEnter}
+                        />
+                    </Row>
+                </AutoColumn>
+                <Footer>
+                    <Button
+                        scale="sm"
+                        variant="text"
+                        //   onClick={() => setModalView(CurrencyModalView.manage)}
+                        className="list-token-manage-button"
+                    >
+                        {t('Add')}
+                    </Button>
+                </Footer>
+            </StyledModalBody>
+        </StyledModalContainer>
+    )
 }
