a95d6fb076dfeb5302af0f7422a8109b377483ba
sphinx <corasphinxy@gmail.com>
2021-11-27 13:58:06 +0000

    Recover GlobalSetting icon

--- src/components/Menu/GlobalSettings/index.tsx
+++ src/components/Menu/GlobalSettings/index.tsx
@@ -6,12 +6,11 @@ const GlobalSettings = () => {
   const [onPresentSettingsModal] = useModal(<SettingsModal />)
 
   return (
-    null
-    // <Flex>
-    //   <IconButton onClick={onPresentSettingsModal} variant="text" scale="sm" mr="8px">
-    //     <CogIcon height={22} width={22} color="textSubtle" />
-    //   </IconButton>
-    // </Flex>
+    <Flex>
+      <IconButton onClick={onPresentSettingsModal} variant="text" scale="sm" mr="8px">
+        <CogIcon height={22} width={22} color="textSubtle" />
+      </IconButton>
+    </Flex>
   )
 }
 
